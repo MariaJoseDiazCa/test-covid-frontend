49a45f43c65fdfc38f0d1c8828b28a3444676612
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2020-08-14 20:24:08 -0400

    Updates

---
+++ precache-manifest.d3708bbe067771f6a44ca4469a8023cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929254064a977dfff4371694692da980",
+    "url": "/test-covid-frontend/index.html"
+  },
+  {
+    "revision": "848d91185c03a8716177",
+    "url": "/test-covid-frontend/static/css/main.2cc18abe.chunk.css"
+  },
+  {
+    "revision": "dc7370c28888de611b31",
+    "url": "/test-covid-frontend/static/js/2.33463610.chunk.js"
+  },
+  {
+    "revision": "9110c923800987d12d856da8548c4502",
+    "url": "/test-covid-frontend/static/js/2.33463610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848d91185c03a8716177",
+    "url": "/test-covid-frontend/static/js/main.279d9a59.chunk.js"
+  },
+  {
+    "revision": "52cfeaf9cd5bb1f0fb7c",
+    "url": "/test-covid-frontend/static/js/runtime-main.afcd5b2a.js"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/test-covid-frontend/static/media/map-image.f40ed5b1.png"
+  }
+]);
